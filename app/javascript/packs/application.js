623c46d36123bfc20729c7b386b49489fd54c6fd
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-09-11 03:18:20 +0000

    [Update]topページレイアウト追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,12 @@ import "channels"
 import "jquery";
 import "popper.js";
 import "bootstrap";
-import "../stylesheets/application" 
-import '@fortawesome/fontawesome-free/js/all'
+import "../stylesheets/application";
+import "@fortawesome/fontawesome-free/js/all"
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 
 Rails.start()
 Turbolinks.start()
