7b96999ecb503e77d05eb363f0e1888b915c4b73
MoeNakashima <111489156+moenakashima@users.noreply.github.com>
2022-09-14 06:24:51 +0000

    [update]CRUD実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+ 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
 
   def index
     @user = current_user
     @users = User.all
   end
+  
+  
+  private 
+    def user_params
+    params.require(:user).permit(:title, :profile_image, :introduction)
+  end
+  
 end
